2975053bb51d75f5249043ac8b58e7cb1a93b1a4
Crypoune <a.messenet@gmail.com>
2026-01-14 17:58:36 +0100

    feat: implement script that print alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-	print("{:s}".format(chr(letter)), end="")
+def print_alphabet():
+	"""Print the alphabet in lowercase."""
+	for letter in range(ord('a'), ord('z') + 1):
+		print(chr(letter), end='')
+	print()  # for a new line after printing the alphabet
