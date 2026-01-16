714465ed0823cfeedd628cec19ac7e6b1ec29277
Crypoune <a.messenet@gmail.com>
2026-01-16 08:48:22 +0100

    feat: implement program that prints number and list of its arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    argc = len(sys.argv) - 1
+    argc = len(argv) - 1
 
     print("{} {}{}".format(
         argc,
         "argument" if argc == 1 else "arguments",
-        "." if argc == 0 else ":"
+        "." if argc < 1 else ":"
     ))
 
-    for i in range(1, argc + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+    if argc >= 1:
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
