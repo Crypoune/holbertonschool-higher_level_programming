3e7fe2cb6083e580f86b8e93d714fadb52a7b5ab
Crypoune <a.messenet@gmail.com>
2026-01-22 22:06:45 +0100

    feat: implement function that adds 2 ints

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+This module provides a function that adds two integers.
+"""
+
+
 def add_integer(a, b=98):
+    """Return the addition of a and b as integers.
+
+    a and b must be integers or floats. Floats are cast to integers before
+    performing the addition.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
