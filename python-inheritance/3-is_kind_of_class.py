92e1b2a1fedafdf17de916f8fb8c324ea314d2b7
Crypoune <a.messenet@gmail.com>
2026-02-04 14:43:21 +0100

    feat(is_kind_of_class): add function to check instance or inheritance

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -7,14 +7,16 @@ or if it inherits from, a specified class.
 
 def is_kind_of_class(obj, a_class):
     """
-    Check if the object is an instance of, or inherits from, the specified class.
+    Check if the object is an instance of,
+    or inherits from, the specified class.
 
     Args:
         obj: The object to test.
         a_class: The class to check against.
 
     Returns:
-        bool: True if obj is an instance of a_class or its subclasses, otherwise False.
+        bool: True if obj is an instance of a_class
+        or its subclasses, otherwise False.
     """
     if isinstance(obj, a_class):
         return True
