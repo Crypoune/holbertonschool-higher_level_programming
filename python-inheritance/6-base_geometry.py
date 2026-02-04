31dedb85c3edc0bf9c31240168f968aedb772605
Crypoune <a.messenet@gmail.com>
2026-02-04 15:14:32 +0100

    feat(BaseGeometry): add area() method to raise exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    This module defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+        An empty class representing base geometry.
+    """
+    def area(self):
+        """
+            Raises an Exception with the message "area() is not implemented".
+        """
+        raise Exception("area() is not implemented")
