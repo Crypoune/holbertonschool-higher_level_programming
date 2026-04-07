ff492cfda0f0cf491e638c2904e75d57a0b6ac34
Crypoune <a.messenet@gmail.com>
2026-04-07 20:14:17 +0200

    fix: wrong repo for this one

--- python_variable_annotations/2-floor.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Module that provides a floor function with type annotations."""
-import math
-
-
-def floor(n: float) -> int:
-    """Return the floor of a float."""
-    return math.floor(n)
